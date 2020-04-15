58c54f919d8c295631be80807a1c3b4d9b27b0ff
Jesus Contreras <jesuscontreras1996102@gmail.com>
2020-04-15 16:26:42 -0500

    add product detail to modal home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,10 @@ class HomeController < ApplicationController
   def index
     @top_products = Spree::Product.last(6)
   end
+
+  def select
+    @selected =  Spree::Product.find(params[:id])
+    @prod = {prod: @selected ,img: @selected.display_image.attachment(:large), price: @selected.display_price}
+    render json: @prod
+  end
 end
